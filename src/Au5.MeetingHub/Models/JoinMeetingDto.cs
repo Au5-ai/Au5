adbd03b20af6617bf1e2f24cc58cbfaf9059996f
MohammadKarimi <mha.karimi@gmail.com>
2025-05-24 22:33:45 +1000

    Add Platform to JoinMeetingDto, MessagePack support, Header
    Added Platform property to JoinMeetingDto for meeting platform info. Applied [MessagePackObject(keyAsPropertyName: true)] to JoinMeetingDto, Message, and new Header class for MessagePack serialization. Introduced Header class with Type property for message metadata.

--- src/Au5.MeetingHub/Models/JoinMeetingDto.cs
+++ src/Au5.MeetingHub/Models/JoinMeetingDto.cs
@@ -3,6 +3,7 @@
 [MessagePackObject(keyAsPropertyName: true)]
 public class JoinMeetingDto
 {
+    public string Platform { get; set; }
     public string MeetingId { get; set; }
     public User User { get; set; }
 }
--- src/Au5.MeetingHub/Models/Message.cs
+++ src/Au5.MeetingHub/Models/Message.cs
@@ -1,10 +1,13 @@
 ﻿namespace Au5.MeetingHub.Models;
+
+[MessagePackObject(keyAsPropertyName: true)]
 public class Message
 {
     public Header Header { get; set; }
     public object Payload { get; set; }
 }
 
+[MessagePackObject(keyAsPropertyName: true)]
 public class Header
 {
     public string Type { get; set; }
