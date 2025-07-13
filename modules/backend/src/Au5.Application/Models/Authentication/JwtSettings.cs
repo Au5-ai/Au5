39d326f43fcfa6a6a13319772711c1f268c0aded
MohammadKarimi <mha.karimi@gmail.com>
2025-07-13 19:53:26 +1000

    Some Refactoring.

--- modules/backend/src/Au5.Application/Models/Authentication/JwtSettings.cs
+++ modules/backend/src/Au5.Application/Models/Authentication/JwtSettings.cs
@@ -2,11 +2,11 @@ namespace Au5.Application.Models.Authentication;
 
 public class JwtSettings
 {
-	public string SecretKey { get; set; } = default!;
+	public string SecretKey { get; set; }
 
-	public string Issuer { get; set; } = default!;
+	public string Issuer { get; set; }
 
-	public string Audience { get; set; } = default!;
+	public string Audience { get; set; }
 
 	public int ExpiryMinutes { get; set; }
 }
--- modules/backend/src/Au5.Application/Models/Authentication/LoginRequestDto.cs
+++ modules/backend/src/Au5.Application/Models/Authentication/LoginRequestDto.cs
@@ -1,8 +1,3 @@
 namespace Au5.Application.Models.Authentication;
 
-public class LoginRequestDto
-{
-	public string Username { get; set; } = string.Empty;
-
-	public string Password { get; set; } = string.Empty;
-}
+public record LoginRequestDto(string Username, string Password);
