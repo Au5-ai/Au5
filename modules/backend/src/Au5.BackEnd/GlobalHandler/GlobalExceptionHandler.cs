6aedb0c320592148b5d9660326b6a862dbe65fa0
MohammadKarimi <mha.karimi@gmail.com>
2025-07-15 17:38:51 +1000

    Add Fluent Validation Exception + Global Handler.

---
+++ modules/backend/src/Au5.BackEnd/GlobalHandler/GlobalExceptionHandler.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace Au5.BackEnd.GlobalHandler;
+
+internal sealed class GlobalExceptionHandler(IProblemDetailsService problemDetailsService) : IExceptionHandler
+{
+	public async ValueTask<bool> TryHandleAsync(
+		HttpContext httpContext,
+		Exception exception,
+		CancellationToken cancellationToken)
+	{
+		httpContext.Response.StatusCode = exception switch
+		{
+			ApplicationException => StatusCodes.Status400BadRequest,
+			_ => StatusCodes.Status500InternalServerError
+		};
+
+		return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+		{
+			HttpContext = httpContext,
+			Exception = exception,
+			ProblemDetails = new ProblemDetails
+			{
+				Type = exception.GetType().Name,
+				Title = "An error occured",
+				Detail = exception.Message
+			}
+		});
+	}
+}
