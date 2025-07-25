181232c6f4124f8b3bc381047ef55233cbb9456d
Mhmdrzo <m.khani2021@gmail.com>
2025-07-25 19:42:20 +0330

    fix errors on tokenServiceTests

--- modules/backend/src/Au5.Application/ConfigServices.cs
+++ modules/backend/src/Au5.Application/ConfigServices.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using Au5.Application.Common.Abstractions;
 using Au5.Application.Common.Piplines;
-using Au5.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Au5.Application;
@@ -19,8 +18,6 @@ public static class ConfigureServices
 
 		services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 		services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
-
-		services.AddSingleton<IJwtBlacklistService, JwtBlacklistService>();
 
 		return services;
 	}
--- modules/backend/src/Au5.BackEnd/Controllers/AuthenticationController.cs
+++ modules/backend/src/Au5.BackEnd/Controllers/AuthenticationController.cs
@@ -1,9 +1,10 @@
+using Au5.Application.Common.Abstractions;
 using Au5.Application.Features.Authentication;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Au5.BackEnd.Controllers;
 
-public class AuthenticationController(ISender mediator) : BaseController
+public class AuthenticationController(ISender mediator, ITokenService tokenService) : BaseController
 {
 	[HttpPost("login")]
 	[AllowAnonymous]
@@ -11,5 +12,18 @@ public class AuthenticationController(ISender mediator) : BaseController
 	public async Task<IActionResult> Login([FromBody] LoginRequest requestModel, CancellationToken ct)
 	{
 		return Ok(await mediator.Send(requestModel, ct));
+	}
+
+	[HttpPost("logout")]
+	public async Task<IActionResult> Logout()
+	{
+		var userId = User.FindFirst("sub")?.Value;
+		var jti = User.FindFirst("jti")?.Value;
+		var expUnix = long.Parse(User.FindFirst("exp").Value);
+		var expiry = DateTimeOffset.FromUnixTimeSeconds(expUnix).UtcDateTime;
+
+		await tokenService.BlacklistTokenAsync(userId, jti, expiry);
+
+		return Ok("Logged out");
 	}
 }
