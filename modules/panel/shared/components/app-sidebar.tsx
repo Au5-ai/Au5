5d64c3ee7098ce5c70f9544226c8b27df7475e6b
mohammadreza-davarzani-82 <74320753+mohammadreza-davarzani-82@users.noreply.github.com>
2025-09-15 21:47:30 +0330

    Reorganize imports and update Logo component usage in AppSidebar

--- modules/panel/shared/components/app-sidebar.tsx
+++ modules/panel/shared/components/app-sidebar.tsx
@@ -1,27 +1,27 @@
 "use client";
 
-import * as React from "react";
-import {
-  ArchiveIcon,
-  Brain,
-  ClosedCaption,
-  Frame,
-  Settings,
-  UserPlus,
-  Waypoints,
-} from "lucide-react";
 import { NavMain } from "@/shared/components/nav-main";
 import { NavSpaces } from "@/shared/components/nav-spaces";
 import { NavUser } from "@/shared/components/nav-user";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenuButton,
+    SidebarRail,
+} from "@/shared/components/ui/sidebar";
 import { User } from "@/shared/types";
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenuButton,
-  SidebarRail,
-} from "@/shared/components/ui/sidebar";
+    ArchiveIcon,
+    Brain,
+    ClosedCaption,
+    Frame,
+    Settings,
+    UserPlus,
+    Waypoints,
+} from "lucide-react";
+import * as React from "react";
 import Logo from "./logo";
 
 // This is sample data.
@@ -120,9 +120,7 @@ export function AppSidebar({ user, ...props }: AppSidebarProps) {
           size="lg"
           className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
           <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-            <div className="bg-primary text-primary-foreground flex size-8 items-center justify-center rounded-md">
-              <Logo />
-            </div>
+            <Logo width={32} height={32} />
           </div>
           <div className="grid flex-1 text-left text-sm leading-tight">
             <span className="truncate font-medium">
