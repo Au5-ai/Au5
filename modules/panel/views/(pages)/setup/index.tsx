f6d05e68e45ad358cb9244093e8e3667e68a300e
MohammadKarimi <mha.karimi@gmail.com>
2025-09-16 23:51:10 +0330

    Fix Sonner UI

--- modules/panel/views/(pages)/setup/index.tsx
+++ modules/panel/views/(pages)/setup/index.tsx
@@ -55,7 +55,7 @@ export default function SignupPage() {
   return (
     <div className="gradient-bg bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <Logo href="#" text="Au5.ai" />
+        <Logo href="#" text={GLOBAL_CAPTIONS.brandName} />
         <SignupForm />
       </div>
     </div>
