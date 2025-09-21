cb2fb5e1601f6e6026f82a46a8787675f18559d2
mo.davarzani <mo.davarzani@asax.ir>
2025-09-21 10:52:18 +0330

    Add global using directives for application abstractions and Entity Framework Core in unit tests

--- modules/backend/tests/Au5.UnitTests/GlobalUsing.cs
+++ modules/backend/tests/Au5.UnitTests/GlobalUsing.cs
@@ -1,6 +1,8 @@
 global using System.Security.Claims;
 global using System.Text;
+global using Au5.Application.Common.Abstractions;
 global using Au5.Domain.Common;
 global using Au5.Infrastructure.Authentication;
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Options;
 global using Moq;
