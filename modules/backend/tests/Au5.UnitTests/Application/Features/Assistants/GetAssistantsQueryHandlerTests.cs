e2966e55a95a87de0370d67d54934b6601255384
MohammadKarimi <mha.karimi@gmail.com>
2025-09-28 22:58:52 +0330

    Fix Unit Tests

--- modules/backend/tests/Au5.UnitTests/Application/Features/Assistants/GetAssistantsQueryHandlerTests.cs
+++ modules/backend/tests/Au5.UnitTests/Application/Features/Assistants/GetAssistantsQueryHandlerTests.cs
@@ -20,7 +20,31 @@ public class GetAssistantsQueryHandlerTests
 		dbContextMock.Setup(x => x.Set<Assistant>()).Returns(dbSetMock.Object);
 
 		var handler = new GetAssistantsQueryHandler(dbContextMock.Object);
-		var result = await handler.Handle(new GetAssistantsQuery(), CancellationToken.None);
+		var result = await handler.Handle(new GetAssistantsQuery(null), CancellationToken.None);
+
+		Assert.True(result.IsSuccess);
+		Assert.Equal(3, result.Data.Count);
+		Assert.Contains(result.Data, x => x.Name == "A1");
+		Assert.Contains(result.Data, x => x.Name == "A2");
+		Assert.Contains(result.Data, x => x.Name == "A3");
+	}
+
+	[Fact]
+	public async Task Should_ReturnActiveAssistants_When_Exist()
+	{
+		var assistants = new List<Assistant>
+		{
+			new() { Id = Guid.NewGuid(), Name = "A1", IsActive = true },
+			new() { Id = Guid.NewGuid(), Name = "A2", IsActive = true },
+			new() { Id = Guid.NewGuid(), Name = "A3", IsActive = false },
+			new() { Id = Guid.NewGuid(), Name = "A4", IsActive = true, IsDefault = true }
+		};
+		var dbSetMock = assistants.BuildMockDbSet();
+		var dbContextMock = new Mock<IApplicationDbContext>();
+		dbContextMock.Setup(x => x.Set<Assistant>()).Returns(dbSetMock.Object);
+
+		var handler = new GetAssistantsQueryHandler(dbContextMock.Object);
+		var result = await handler.Handle(new GetAssistantsQuery(true), CancellationToken.None);
 
 		Assert.True(result.IsSuccess);
 		Assert.Equal(2, result.Data.Count);
