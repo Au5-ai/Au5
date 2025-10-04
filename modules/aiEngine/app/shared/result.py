f932b6e26dc4e8fee90f39874306fc50130141d2
Milad <milad.liri@yahoo.com>
2025-10-05 00:50:57 +0330

    update result pattern

--- modules/aiEngine/app/shared/result.py
+++ modules/aiEngine/app/shared/result.py
@@ -6,17 +6,17 @@ E = TypeVar("E")
 
 class Result(BaseModel, Generic[T, E]):
     status: int
-    success: bool
+    is_success: bool
     data: Union[T, None] = None
     error: Union[E, None] = None
 
     @staticmethod
     def success(data: T) -> "Result[T, None]":
-        return Result(success=True, data=data, status=200)
+        return Result(is_success=True, data=data, status=200)
 
     @staticmethod
     def failure(error: E, status: int = 400) -> "Result[None, E]":
-        return Result(success=False, error=error, status=status)
+        return Result(is_success=False, error=error, status=status)
 
     def is_failure(self) -> bool:
         return not self.is_success
