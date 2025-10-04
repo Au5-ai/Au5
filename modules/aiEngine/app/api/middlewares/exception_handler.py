7d8b9b69db4fb7219ae2286dc7ecc32eea4f67b2
Milad <milad.liri@yahoo.com>
2025-10-04 12:59:57 +0330

    update exception handler to handle app exception

--- modules/aiEngine/app/api/middlewares/exception_handler.py
+++ modules/aiEngine/app/api/middlewares/exception_handler.py
@@ -1,9 +1,34 @@
 from fastapi import Request, Response
+from fastapi.responses import JSONResponse
 from app.shared.result import Result
+from app.shared.exceptions import AppException
+import logging
 
-async def exception_handler(request: Request, call_next):
+logger = logging.getLogger(__name__) 
+
+async def exception_handler(request: Request, call_next) -> JSONResponse :
     try:
         return await call_next(request)
+
+    except AppException as ae:
+        logger.error(
+            f"AppException: {ae.message}",
+            extra={
+                "path": request.url.path,
+                "method": request.method,
+                "status_code": ae.status_code,
+                "details": ae.details
+            }
+        )
+        return JSONResponse(content=ae.to_result(), status_code=ae.status_code, media_type="application/json")
+
     except Exception as e:
-        error_result = Result.fail(str(e))
-        return Response(content=error_result.json(), status_code=500, media_type="application/json")
+        logger.error(
+            f"Exception: {ae.message}",
+            extra={
+                "path": request.url.path,
+                "method": request.method,
+            }
+        )
+        return JSONResponse(content=Result.fail(str(e)), status_code=500, media_type="application/json")
+
